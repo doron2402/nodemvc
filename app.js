8d398a09a0082aaa413197e6812b88728883e517
Doron Segal <doron2402@gmail.com>
2014-01-25 17:52:32 -0800

    adding server.js

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var Routes = require('./routes'),
 	Hapi = require('hapi'),
-	Server = new Hapi.Server('localhost', 8000, { cors: true, maxSockets: 10 });
+	Options = {
+		cors: true, 
+		maxSockets: 10
+	},
+	Server = new Hapi.Server('localhost', 8000, Options);
 
 Server.start(function () {
 
     console.log('Server started at: ' + Server.info.uri);
 });
+
+var hello = function(req) {
+
+	this.reply('Success').state('session', session);
+}
+
+Server.route({
+	method: 'GET',
+	path: '/',
+	config: {
+		handler: hello
+	}
+});
+
